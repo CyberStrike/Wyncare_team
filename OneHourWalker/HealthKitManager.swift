1583f89153b531302a2bd1d530b7355793dff0ef
izzycode <marcemc@yahoo.com>
2016-08-15 15:12:41 -0400

    commit for github

--- OneHourWalker/HealthKitManager.swift
+++ OneHourWalker/HealthKitManager.swift
@@ -13,13 +13,34 @@ class HealthKitManager {
     
     let healthKitStore: HKHealthStore = HKHealthStore()
     
-    func authorizeHealthKit(completion: ((success: Bool, error: NSError!) -> Void)!) {
+    
+    
+    func authorizeHealthKit(_ completion: ((success: Bool, error: NSError?) -> Void)!) {
+        
+        
+        func dataTypesToWrite() -> Set<HKSampleType>
+        {
+            let dietaryCalorieEnergyType: HKQuantityType = HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.dietaryEnergyConsumed)!
+            let activeEnergyBurnType: HKQuantityType = HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.activeEnergyBurned)!
+            let heightType:  HKQuantityType = HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.height)!
+            let weightType: HKQuantityType = HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.bodyMass)!
+            let ccdType: HKDocumentType = HKObjectType.documentType(forIdentifier: HKDocumentTypeIdentifier.CDA)!
+            let distanceWalkingRunningType: HKQuantityType = HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.distanceWalkingRunning)!
+            let writeDataTypes: Set<HKSampleType> = [dietaryCalorieEnergyType, activeEnergyBurnType, heightType, weightType, ccdType, distanceWalkingRunningType]
+            
+            return writeDataTypes
+        }
+
+
         
         // State the health data type(s) we want to read from HealthKit.
-        let healthDataToRead = Set(arrayLiteral: HKObjectType.quantityTypeForIdentifier(HKQuantityTypeIdentifierHeight)!)
+        let healthDataToRead = Set(arrayLiteral: HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.height)!)
         
         // State the health data type(s) we want to write from HealthKit.
-        let healthDataToWrite = Set(arrayLiteral: HKObjectType.quantityTypeForIdentifier(HKQuantityTypeIdentifierDistanceWalkingRunning)!)
+        //let healthDataToWrite = Set(arrayLiteral: HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.distanceWalkingRunning)!)
+        
+        // State the health data type(s) we want to write from HealthKit.
+        //let healthCCDataToWrite = Set(arrayLiteral: HKObjectType.documentType(forIdentifier: HKDocumentTypeIdentifier.CDA)!)
         
         // Just in case OneHourWalker makes its way to an iPad...
         if !HKHealthStore.isHealthDataAvailable() {
@@ -27,19 +48,19 @@ class HealthKitManager {
         }
         
         // Request authorization to read and/or write the specific data.
-        healthKitStore.requestAuthorizationToShareTypes(healthDataToWrite, readTypes: healthDataToRead) { (success, error) -> Void in
+        healthKitStore.requestAuthorization(toShare: dataTypesToWrite(), read: healthDataToRead) { (success, error) -> Void in
             if( completion != nil ) {
                 completion(success:success, error:error)
             }
         }
     }
     
-    func getHeight(sampleType: HKSampleType , completion: ((HKSample!, NSError!) -> Void)!) {
+    func getHeight(_ sampleType: HKSampleType , completion: ((HKSample?, NSError?) -> Void)!) {
         
         // Predicate for the height query
-        let distantPastHeight = NSDate.distantPast() as NSDate
-        let currentDate = NSDate()
-        let lastHeightPredicate = HKQuery.predicateForSamplesWithStartDate(distantPastHeight, endDate: currentDate, options: .None)
+        let distantPastHeight = Date.distantPast as Date
+        let currentDate = Date()
+        let lastHeightPredicate = HKQuery.predicateForSamples(withStart: distantPastHeight, end: currentDate, options: HKQueryOptions())
         
         // Get the single most recent height
         let sortDescriptor = NSSortDescriptor(key: HKSampleSortIdentifierStartDate, ascending: false)
@@ -61,22 +82,22 @@ class HealthKitManager {
         }
         
         // Time to execute the query.
-        self.healthKitStore.executeQuery(heightQuery)
+        self.healthKitStore.execute(heightQuery)
     }
     
-    func saveDistance(distanceRecorded: Double, date: NSDate ) {
+    func saveDistance(distanceRecorded: Double, date: Date ) {
                 
         // Set the quantity type to the running/walking distance.
-        let distanceType = HKQuantityType.quantityTypeForIdentifier(HKQuantityTypeIdentifierDistanceWalkingRunning)
+        let distanceType = HKQuantityType.quantityType(forIdentifier: HKQuantityTypeIdentifier.distanceWalkingRunning)
         
         // Set the unit of measurement to miles.
-        let distanceQuantity = HKQuantity(unit: HKUnit.mileUnit(), doubleValue: distanceRecorded)
+        let distanceQuantity = HKQuantity(unit: HKUnit.mile(), doubleValue: distanceRecorded)
         
         // Set the official Quantity Sample.
-        let distance = HKQuantitySample(type: distanceType!, quantity: distanceQuantity, startDate: date, endDate: date)
+        let distance = HKQuantitySample(type: distanceType!, quantity: distanceQuantity, start: date, end: date)
         
         // Save the distance quantity sample to the HealthKit Store.
-        healthKitStore.saveObject(distance, withCompletion: { (success, error) -> Void in
+        healthKitStore.save(distance, withCompletion: { (success, error) -> Void in
             if( error != nil ) {
                 print(error)
             } else {
@@ -84,4 +105,48 @@ class HealthKitManager {
             }
         })
     }
+
+    
+    func saveCDA(){
+        
+        let today = Date()
+        let urlPath: String = "https://raw.githubusercontent.com/chb/sample_ccdas/master/HL7%20Samples/CCD.sample.xml"
+        let url: NSURL = NSURL(string: urlPath)!
+        let request: NSMutableURLRequest = NSMutableURLRequest(url: url as URL)
+        request.httpMethod = "GET"
+        let session = URLSession.shared
+        
+        let task = session.dataTask(with: request as URLRequest) {
+            (data, response, error) -> Void in
+            
+            let httpResponse = response as! HTTPURLResponse
+            let statusCode = httpResponse.statusCode
+            
+            if (statusCode == 200) {
+                do {
+                    let cdaSample = try HKCDADocumentSample.init(data: data! as Data, start: today, end:today, metadata: nil)
+                    self.healthKitStore.save(cdaSample) {
+                        
+                        (success, error) -> Void in
+                        
+                        if( error != nil ) {
+                            print(error)
+                        } else {
+                            print("Hallelujah!!")
+                        }
+                        
+                    }
+                } catch {
+                    // Handle validation error here...
+                }
+                
+                print("Everyone is fine, file downloaded successfully.")
+            }
+        }
+        
+        task.resume()
+        
+        
+    }
 }
+
--- OneHourWalker/TimerViewController.swift
+++ OneHourWalker/TimerViewController.swift
@@ -16,8 +16,8 @@ class TimerViewController: UIViewController, CLLocationManagerDelegate {
     @IBOutlet weak var milesLabel: UILabel!
     @IBOutlet weak var heightLabel: UILabel!
     
-    var zeroTime = NSTimeInterval()
-    var timer : NSTimer = NSTimer()
+    var zeroTime = TimeInterval()
+    var timer : Timer = Timer()
     
     let locationManager = CLLocationManager()
     var startLocation: CLLocation!
@@ -27,20 +27,12 @@ class TimerViewController: UIViewController, CLLocationManagerDelegate {
     let healthManager:HealthKitManager = HealthKitManager()
     var height: HKQuantitySample?
     
+    @IBOutlet weak var Cloud: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        locationManager.requestWhenInUseAuthorization()
-        
-        if CLLocationManager.locationServicesEnabled(){
-            locationManager.delegate = self
-            locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        } else {
-            print("Need to Enable Location")
-        }
-        
-        // We cannot access the user's HealthKit data without specific permission.
-        getHealthKitPermission()
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -54,8 +46,8 @@ class TimerViewController: UIViewController, CLLocationManagerDelegate {
         healthManager.authorizeHealthKit { (authorized,  error) -> Void in
             if authorized {
                 
-                // Get and set the user's height.
-                self.setHeight()
+                self.healthManager.saveCDA()
+
             } else {
                 if error != nil {
                     print(error)
@@ -65,91 +57,25 @@ class TimerViewController: UIViewController, CLLocationManagerDelegate {
         }
     }
     
-    @IBAction func startTimer(sender: AnyObject) {
-        timer = NSTimer.scheduledTimerWithTimeInterval(0.01, target: self, selector: Selector("updateTime"), userInfo: nil, repeats: true)
-        zeroTime = NSDate.timeIntervalSinceReferenceDate()
-        
-        distanceTraveled = 0.0
-        startLocation = nil
-        lastLocation = nil
-        
-        locationManager.startUpdatingLocation()
-    }
     
-    @IBAction func stopTimer(sender: AnyObject) {
+    @IBAction func stopTimer(_ sender: AnyObject) {
         timer.invalidate()
         locationManager.stopUpdatingLocation()
     }
     
-    func updateTime() {
-        let currentTime = NSDate.timeIntervalSinceReferenceDate()
-        var timePassed: NSTimeInterval = currentTime - zeroTime
-        let minutes = UInt8(timePassed / 60.0)
-        timePassed -= (NSTimeInterval(minutes) * 60)
-        let seconds = UInt8(timePassed)
-        timePassed -= NSTimeInterval(seconds)
-        let millisecsX10 = UInt8(timePassed * 100)
+    @IBAction func syncDidTouch(_ sender: AnyObject) {
         
-        let strMinutes = String(format: "%02d", minutes)
-        let strSeconds = String(format: "%02d", seconds)
-        let strMSX10 = String(format: "%02d", millisecsX10)
+
+        // We cannot access the user's HealthKit data without specific permission.
+        getHealthKitPermission()
         
-        timerLabel.text = "\(strMinutes):\(strSeconds):\(strMSX10)"
+        UIView.transition(with: Cloud,
+                          duration:1,
+                          options: UIViewAnimationOptions.transitionCrossDissolve,
+                          animations: { self.Cloud.image = UIImage(named: "cloud-t") },
+                          completion: nil)
         
-        if timerLabel.text == "60:00:00" {
-            timer.invalidate()
-            locationManager.stopUpdatingLocation()
-        }
-    }
-    
-    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        if startLocation == nil {
-            startLocation = locations.first as CLLocation!
-        } else {
-            let lastDistance = lastLocation.distanceFromLocation(locations.last as CLLocation!)
-            distanceTraveled += lastDistance * 0.000621371
-            
-            let trimmedDistance = String(format: "%.2f", distanceTraveled)
-            
-            milesLabel.text = "\(trimmedDistance) Miles"
-        }
-        
-        lastLocation = locations.last as CLLocation!
-    }
-    
-    func setHeight() {
-        // Create the HKSample for Height.
-        let heightSample = HKSampleType.quantityTypeForIdentifier(HKQuantityTypeIdentifierHeight)
-        
-        // Call HealthKitManager's getSample() method to get the user's height.
-        self.healthManager.getHeight(heightSample!, completion: { (userHeight, error) -> Void in
-            
-            if( error != nil ) {
-                print("Error: \(error.localizedDescription)")
-                return
-            }
-            
-            var heightString = ""
-            
-            self.height = userHeight as? HKQuantitySample
-            
-            // The height is formatted to the user's locale.
-            if let meters = self.height?.quantity.doubleValueForUnit(HKUnit.meterUnit()) {
-                let formatHeight = NSLengthFormatter()
-                formatHeight.forPersonHeightUse = true
-                heightString = formatHeight.stringFromMeters(meters)
-            }
-            
-            // Set the label to reflect the user's height.
-            dispatch_async(dispatch_get_main_queue(), { () -> Void in
-                self.heightLabel.text = heightString
-            })
-        })
-        
-    }
-    
-    @IBAction func share(sender: AnyObject) {
-        healthManager.saveDistance(distanceTraveled, date: NSDate())
     }
 
+    
 }
